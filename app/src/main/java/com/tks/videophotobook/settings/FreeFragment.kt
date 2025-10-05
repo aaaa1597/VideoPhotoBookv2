dc97540b446818b466caff81a4680648bba2f8cf
aaaa1597 <tokushi2011@gmail.com>
2025-10-05 21:33:04 +0900

    Implementation of file selection. ファイル選択処理実装。

--- app/src/main/java/com/tks/videophotobook/settings/FreeFragment.kt
+++ app/src/main/java/com/tks/videophotobook/settings/FreeFragment.kt
@@ -1,7 +1,11 @@
 package com.tks.videophotobook.settings
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.provider.DocumentsContract
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +14,7 @@ import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.activityViewModels
@@ -32,8 +37,19 @@ class FreeFragment : Fragment() {
     private lateinit var _binding: FragmentFreeBinding
     private val _viewModel: SettingViewModel by activityViewModels()
     private lateinit var _markerVideoSetAdapter: MarkerVideoSetAdapter
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+    /* ファイル選択ランチャー */
+    private val _pickFileLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        /* ファイルリストの戻り */
+        result ->
+            if (result.resultCode != Activity.RESULT_OK) return@registerForActivityResult
+            if (result.data==null) return@registerForActivityResult
+            if (result.data!!.data == null) return@registerForActivityResult
+
+            /* 単一のファイルが選択された */
+            val uri = result.data!!.data ?: return@registerForActivityResult
+            Log.d("aaaaa", "file URI: $uri")
+            /*  ここで選択されたファイルURIを使って何かする */
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
@@ -88,16 +104,32 @@ class FreeFragment : Fragment() {
 
         dialogView.findViewById<TextView>(R.id.etv_comment).text = item.comment
 
+        val getFileUri: (String) -> Unit = {
+                mimeType ->
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                /* 単一ファイル選択なのでコメント化 */
+                //              putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+                /* 選択可能なMIMEタイプを指定 */
+                type = mimeType
+                /* EXTRA_INITIAL_URIを設定して、初期表示ディレクトリを指定 */
+                putExtra(DocumentsContract.EXTRA_INITIAL_URI, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            }
+            /* 生成Intentで起動 */
+            _pickFileLauncher.launch(intent)
+        }
+
         /* マーカー画像設定 */
         dialogView.findViewById<ImageView>(R.id.igv_markerpreview).setOnClickListener {
-            TODO("マーカー画像設定 処理")
+            getFileUri("image/*")
         }
+
         /* 再生動画設定 */
         dialogView.findViewById<ImageView>(R.id.imv_video_thumbnail2).setOnClickListener {
-            TODO("再生動画設定 処理")
+            getFileUri("video/*")
         }
         dialogView.findViewById<VideoThumbnailPlayerView>(R.id.pyv_video_thumbnail2).setOnClickListener {
-
+            getFileUri("video/*")
         }
         /* キャンセル */
         dialogView.findViewById<Button>(R.id.btnCancel).setOnClickListener {
@@ -121,76 +153,77 @@ class FreeFragment : Fragment() {
             }
         }
     }
+}
 
-    class MarkerVideoSetAdapter(private val context: Context, private val onItemClicked: (MarkerVideoSet) -> Unit) :
-        ListAdapter<MarkerVideoSet, MarkerVideoSetAdapter.MarkerVideoSetViewHolder>(MarkerVideoSetDiffCallback()) {
+/* Adapterクラス */
+class MarkerVideoSetAdapter(private val context: Context, private val onItemClicked: (MarkerVideoSet) -> Unit) :
+    ListAdapter<MarkerVideoSet, MarkerVideoSetAdapter.MarkerVideoSetViewHolder>(MarkerVideoSetDiffCallback()) {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarkerVideoSetViewHolder {
-            val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.list_item_marker_video, parent, false)
-            return MarkerVideoSetViewHolder(view)
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarkerVideoSetViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item_marker_video, parent, false)
+        return MarkerVideoSetViewHolder(view)
+    }
 
-        override fun onBindViewHolder(holder: MarkerVideoSetViewHolder, position: Int) {
-            val item = getItem(position)
-            holder.bind(context, item, onItemClicked)
-        }
+    override fun onBindViewHolder(holder: MarkerVideoSetViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(context, item, onItemClicked)
+    }
 
-        class MarkerVideoSetViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            private val topCdv: CardView = itemView.findViewById(R.id.cdv_top)
-            private val targetNameTxt: TextView = itemView.findViewById(R.id.txt_targetname)
-            private val targetImageImv: ImageView = itemView.findViewById(R.id.imv_targetImage)
-            private val videoInfoFly: FrameLayout = itemView.findViewById(R.id.fly_videoInfo)
-            private val videoName: TextView = itemView.findViewById(R.id.txt_videoname)
-            private val videothumbnailPyv: VideoThumbnailPlayerView = itemView.findViewById(R.id.pyv_video_thumbnail)
-            private val videothumbnailImv: ImageView = itemView.findViewById(R.id.imv_video_thumbnail)
-            private val commentTxt: TextView = itemView.findViewById(R.id.txt_comment)
+    class MarkerVideoSetViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val topCdv: CardView = itemView.findViewById(R.id.cdv_top)
+        private val targetNameTxt: TextView = itemView.findViewById(R.id.txt_targetname)
+        private val targetImageImv: ImageView = itemView.findViewById(R.id.imv_targetImage)
+        private val videoInfoFly: FrameLayout = itemView.findViewById(R.id.fly_videoInfo)
+        private val videoName: TextView = itemView.findViewById(R.id.txt_videoname)
+        private val videothumbnailPyv: VideoThumbnailPlayerView = itemView.findViewById(R.id.pyv_video_thumbnail)
+        private val videothumbnailImv: ImageView = itemView.findViewById(R.id.imv_video_thumbnail)
+        private val commentTxt: TextView = itemView.findViewById(R.id.txt_comment)
 
-            fun bind(context: Context, item: MarkerVideoSet, onItemClicked: (MarkerVideoSet) -> Unit) {
-                /* ARマーカーID */
-                targetNameTxt.text = item.targetName
-                /* ARマーカー画像 */
-                if(Utils.isUriValid(context, item.targetImageUri))
-                    targetImageImv.setImageURI(item.targetImageUri)
-                else {
-                    item.targetImageUri = "".toUri()
-                    targetImageImv.setImageResource(item.targetImageTemplateResId)
-                }
-                /* 動画名/動画ファイル */
-                if(Utils.isUriValid(context, item.videoUri)) {
-                    videoName.text = Utils.getFileNameFromUri(context, item.videoUri)
-                    videothumbnailImv.visibility = View.GONE
-                    videothumbnailPyv.visibility = View.VISIBLE
-                    videothumbnailPyv.setVideoUri(item.videoUri)
-                }
-                else {
-                    item.videoUri = "".toUri()
-                    videoName.text = context.getString(R.string.video_none)
-                    videothumbnailPyv.visibility = View.GONE
-                    videothumbnailImv.visibility = View.VISIBLE
-                    videothumbnailImv.setImageResource(R.drawable.videofilenotfound)
-                }
+        fun bind(context: Context, item: MarkerVideoSet, onItemClicked: (MarkerVideoSet) -> Unit) {
+            /* ARマーカーID */
+            targetNameTxt.text = item.targetName
+            /* ARマーカー画像 */
+            if(Utils.isUriValid(context, item.targetImageUri))
+                targetImageImv.setImageURI(item.targetImageUri)
+            else {
+                item.targetImageUri = "".toUri()
+                targetImageImv.setImageResource(item.targetImageTemplateResId)
+            }
+            /* 動画名/動画ファイル */
+            if(Utils.isUriValid(context, item.videoUri)) {
+                videoName.text = Utils.getFileNameFromUri(context, item.videoUri)
+                videothumbnailImv.visibility = View.GONE
+                videothumbnailPyv.visibility = View.VISIBLE
+                videothumbnailPyv.setVideoUri(item.videoUri)
+            }
+            else {
+                item.videoUri = "".toUri()
+                videoName.text = context.getString(R.string.video_none)
+                videothumbnailPyv.visibility = View.GONE
+                videothumbnailImv.visibility = View.VISIBLE
+                videothumbnailImv.setImageResource(R.drawable.videofilenotfound)
+            }
 
-                commentTxt.text = item.comment
+            commentTxt.text = item.comment
 
-                /* クリックリスナーの設定 */
-                topCdv.setOnClickListener {
-                    onItemClicked(item)
-                }
+            /* クリックリスナーの設定 */
+            topCdv.setOnClickListener {
+                onItemClicked(item)
             }
         }
     }
+}
 
-    /* リストの変更を検出するためのコールバック */
-    class MarkerVideoSetDiffCallback : DiffUtil.ItemCallback<MarkerVideoSet>() {
-        override fun areItemsTheSame(oldItem: MarkerVideoSet, newItem: MarkerVideoSet): Boolean {
-            /* IDが同じなら同じアイテムと見なす */
-            return oldItem.targetName == newItem.targetName
-        }
+/* リストの変更を検出するためのコールバック */
+class MarkerVideoSetDiffCallback : DiffUtil.ItemCallback<MarkerVideoSet>() {
+    override fun areItemsTheSame(oldItem: MarkerVideoSet, newItem: MarkerVideoSet): Boolean {
+        /* IDが同じなら同じアイテムと見なす */
+        return oldItem.targetName == newItem.targetName
+    }
 
-        override fun areContentsTheSame(oldItem: MarkerVideoSet, newItem: MarkerVideoSet): Boolean {
-            /* 内容が完全に一致するかどうかを確認 */
-            return oldItem == newItem
-        }
+    override fun areContentsTheSame(oldItem: MarkerVideoSet, newItem: MarkerVideoSet): Boolean {
+        /* 内容が完全に一致するかどうかを確認 */
+        return oldItem == newItem
     }
 }

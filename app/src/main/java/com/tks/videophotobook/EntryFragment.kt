f65112f0009f03ae4fece6ceec3b79e5b4a069cd
aaaa1597 <tokushi2011@gmail.com>
2025-10-08 23:04:20 +0900

    設定シーケンスの見直し

--- app/src/main/java/com/tks/videophotobook/EntryFragment.kt
+++ app/src/main/java/com/tks/videophotobook/EntryFragment.kt
@@ -5,19 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.fragment.findNavController
-import com.tks.videophotobook.databinding.FragmentMainBinding
+import com.tks.videophotobook.databinding.FragmentEntryBinding
 import com.tks.videophotobook.settings.MARKER_VIDEO_MAP_JSON
 import java.io.File
 
-class MainFragment : Fragment() {
-    private lateinit var _binding: FragmentMainBinding
+class EntryFragment : Fragment() {
+    private lateinit var _binding: FragmentEntryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
-        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        _binding = FragmentEntryBinding.inflate(inflater, container, false)
         return _binding.root
     }
 
@@ -28,7 +29,14 @@ class MainFragment : Fragment() {
         val file = File(requireContext().externalCacheDir, MARKER_VIDEO_MAP_JSON)
         if ( !file.exists()) {
             /* 存在しない場合、SettingFragmentを表示(マーカー/動画紐付け情報を生成する)*/
-            findNavController().navigate(R.id.action_mainFragment_to_settingFragment_slide)
+            AlertDialog.Builder(requireContext())
+                .setTitle(R.string.no_data)
+                .setMessage(R.string.no_bind_data)
+                .setPositiveButton(android.R.string.ok) { dialog, which ->
+                    dialog.dismiss()
+                    findNavController().navigate(R.id.action_mainFragment_to_settingFragment_slide)
+                }
+                .show()
         }
     }
 }

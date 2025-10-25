0273d8b3eef865041895ee60e983e2e83ab2be83
aaaa1597 <tokushi2011@gmail.com>
2025-10-25 12:19:51 +0900

    こまごま修正

--- app/src/androidTest/java/com/tks/videophotobook/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/tks/videophotobook/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.aaa.permissionsample", appContext.packageName)
+        assertEquals("com.tks.videophotobook", appContext.packageName)
     }
 }
